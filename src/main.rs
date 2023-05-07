200948690f7d16e29d0cd844e7c1d1119de2101c
Bradley Gannon <bradley@bradleygannon.com>
2023-05-06 20:38:57 -0400

    Improve performance of Neighborhood

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,7 @@ use std::fmt;
 
 const N_NODES: usize = 54;
 
-#[derive(Clone)]
-enum Neighborhood {
-    Three(usize, usize, usize),
-    Two(usize, usize),
-    One(usize),
-    None,
-}
-
-impl Neighborhood {
-    fn iter(&self) -> impl Iterator<Item=&usize> {
-        match self {
-            Neighborhood::Three(x, y, z) => vec![x, y, z].into_iter(),
-            Neighborhood::Two(x, y) => vec![x, y].into_iter(),
-            Neighborhood::One(x) => vec![x].into_iter(),
-            Neighborhood::None => vec![].into_iter(),
-        }
-    }
-}
-
-impl fmt::Display for Neighborhood {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Neighborhood::Three(x, y, z) => write!(f, "{{{}, {}, {}}}", x, y, z),
-            Neighborhood::Two(x, y) => write!(f, "{{{}, {}}}", x, y),
-            Neighborhood::One(x) => write!(f, "{{{}}}", x),
-            Neighborhood::None => write!(f, "{{}}"),
-        }
-    }
-}
+type Neighborhood = [Option<usize>; 3];
 
 struct Board {
     neighborhoods: [Neighborhood; N_NODES],
@@ -41,73 +13,67 @@ impl Board {
     fn new() -> Self {
         Self {
             neighborhoods: [
-                Neighborhood::Two(1, 3),         // 0
-                Neighborhood::Two(0, 4),         // 1
-                Neighborhood::Two(3, 7),         // 2
-                Neighborhood::Three(0, 2, 8),    // 3
-                Neighborhood::Three(1, 5, 9),    // 4
-                Neighborhood::Two(4, 10),        // 5
-                Neighborhood::Two(7, 12),        // 6
-                Neighborhood::Three(2, 6, 13),   // 7
-                Neighborhood::Three(3, 9, 14),   // 8
-                Neighborhood::Three(4, 8, 15),   // 9
-
-                Neighborhood::Three(5, 11, 16),  // 10
-                Neighborhood::Two(10, 17),       // 11
-                Neighborhood::Two(6, 18),        // 12
-                Neighborhood::Three(7, 14, 19),  // 13
-                Neighborhood::Three(8, 13, 20),  // 14
-                Neighborhood::Three(9, 16, 21),  // 15
-                Neighborhood::Three(10, 15, 22), // 16
-                Neighborhood::Two(11, 23),       // 17
-                Neighborhood::Three(12, 19, 24), // 18
-                Neighborhood::Three(13, 18, 25), // 19
-
-                Neighborhood::Three(14, 21, 26), // 20
-                Neighborhood::Three(15, 20, 27), // 21
-                Neighborhood::Three(16, 23, 28), // 22
-                Neighborhood::Three(17, 22, 29), // 23
-                Neighborhood::Two(18, 30),       // 24
-                Neighborhood::Three(19, 26, 31), // 25
-                Neighborhood::Three(20, 25, 32), // 26
-                Neighborhood::Three(21, 28, 33), // 27
-                Neighborhood::Three(22, 27, 34), // 28
-                Neighborhood::Two(23, 35),       // 29
-
-                Neighborhood::Three(24, 31, 36), // 30
-                Neighborhood::Three(25, 30, 37), // 31
-                Neighborhood::Three(26, 33, 38), // 32
-                Neighborhood::Three(27, 32, 39), // 33
-                Neighborhood::Three(28, 35, 40), // 34
-                Neighborhood::Three(29, 34, 41), // 35
-                Neighborhood::Two(30, 42),       // 36
-                Neighborhood::Three(31, 38, 43), // 37
-                Neighborhood::Three(32, 37, 44), // 38
-                Neighborhood::Three(33, 40, 45), // 39
-
-                Neighborhood::Three(34, 39, 46), // 40
-                Neighborhood::Two(35, 47),       // 41
-                Neighborhood::Two(36, 43),       // 42
-                Neighborhood::Three(37, 42, 48), // 43
-                Neighborhood::Three(38, 45, 49), // 44
-                Neighborhood::Three(39, 44, 50), // 45
-                Neighborhood::Three(40, 47, 51), // 46
-                Neighborhood::Two(41, 46),       // 47
-                Neighborhood::Two(43, 49),       // 48
-                Neighborhood::Three(44, 48, 52), // 49
-
-                Neighborhood::Three(45, 51, 53), // 50
-                Neighborhood::Two(46, 50),       // 51
-                Neighborhood::Two(49, 53),       // 52
-                Neighborhood::Two(50, 52)        // 53
-
+                [Some(1), Some(3), None],       // 0
+                [Some(0), Some(4), None],       // 1
+                [Some(3), Some(7), None],       // 2
+                [Some(0), Some(2), Some(8)],    // 3
+                [Some(1), Some(5), Some(9)],    // 4
+                [Some(4), Some(10), None],      // 5
+                [Some(7), Some(12), None],      // 6
+                [Some(2), Some(6), Some(13)],   // 7
+                [Some(3), Some(9), Some(14)],   // 8
+                [Some(4), Some(8), Some(15)],   // 9
+                [Some(5), Some(11), Some(16)],  // 10
+                [Some(10), Some(17), None],     // 11
+                [Some(6), Some(18), None],      // 12
+                [Some(7), Some(14), Some(19)],  // 13
+                [Some(8), Some(13), Some(20)],  // 14
+                [Some(9), Some(16), Some(21)],  // 15
+                [Some(10), Some(15), Some(22)], // 16
+                [Some(11), Some(23), None],     // 17
+                [Some(12), Some(19), Some(24)], // 18
+                [Some(13), Some(18), Some(25)], // 19
+                [Some(14), Some(21), Some(26)], // 20
+                [Some(15), Some(20), Some(27)], // 21
+                [Some(16), Some(23), Some(28)], // 22
+                [Some(17), Some(22), Some(29)], // 23
+                [Some(18), Some(30), None],     // 24
+                [Some(19), Some(26), Some(31)], // 25
+                [Some(20), Some(25), Some(32)], // 26
+                [Some(21), Some(28), Some(33)], // 27
+                [Some(22), Some(27), Some(34)], // 28
+                [Some(23), Some(35), None],     // 29
+                [Some(24), Some(31), Some(36)], // 30
+                [Some(25), Some(30), Some(37)], // 31
+                [Some(26), Some(33), Some(38)], // 32
+                [Some(27), Some(32), Some(39)], // 33
+                [Some(28), Some(35), Some(40)], // 34
+                [Some(29), Some(34), Some(41)], // 35
+                [Some(30), Some(42), None],     // 36
+                [Some(31), Some(38), Some(43)], // 37
+                [Some(32), Some(37), Some(44)], // 38
+                [Some(33), Some(40), Some(45)], // 39
+                [Some(34), Some(39), Some(46)], // 40
+                [Some(35), Some(47), None],     // 41
+                [Some(36), Some(43), None],     // 42
+                [Some(37), Some(42), Some(48)], // 43
+                [Some(38), Some(45), Some(49)], // 44
+                [Some(39), Some(44), Some(50)], // 45
+                [Some(40), Some(47), Some(51)], // 46
+                [Some(41), Some(46), None],     // 47
+                [Some(43), Some(49), None],     // 48
+                [Some(44), Some(48), Some(52)], // 49
+                [Some(45), Some(51), Some(53)], // 50
+                [Some(46), Some(50), None],     // 51
+                [Some(49), Some(53), None],     // 52
+                [Some(50), Some(52), None],     // 53
             ],
 
-            deleted: [ false; N_NODES ],
+            deleted: [false; N_NODES],
         }
     }
 
-    fn small() -> Self {
+    fn _small() -> Self {
         let mut board = Self::new();
         board.deleted = [true; 54];
         board.deleted[0] = false;
@@ -121,58 +87,30 @@ impl Board {
 
     // TODO check that this access pattern isn't slow af
     fn get(&self, index: usize) -> Neighborhood {
-        if self.deleted[index] { return Neighborhood::None }
+        if self.deleted[index] {
+            return [None; 3];
+        }
 
-        let neighborhood = &self.neighborhoods[index];
-        match neighborhood {
-            Neighborhood::Three(x, y, z) => {
-                if self.deleted[*x] && self.deleted[*y] && self.deleted[*z] {
-                    return Neighborhood::None;
-                } else if self.deleted[*x] && self.deleted[*y] {
-                    return Neighborhood::One(*z);
-                } else if self.deleted[*x] && self.deleted[*z] {
-                    return Neighborhood::One(*y);
-                } else if self.deleted[*y] && self.deleted[*z] {
-                    return Neighborhood::One(*x);
-                } else if self.deleted[*x] {
-                    return Neighborhood::Two(*y, *z);
-                } else if self.deleted[*y] {
-                    return Neighborhood::Two(*x, *z);
-                } else if self.deleted[*z] {
-                    return Neighborhood::Two(*x, *y);
-                } else {
-                    return Neighborhood::Three(*x, *y, *z);
-                }
-            },
-            Neighborhood::Two(x, y) => {
-                if self.deleted[*x] && self.deleted[*y] {
-                    return Neighborhood::None;
-                } else if self.deleted[*x] {
-                    return Neighborhood::One(*y);
-                } else if self.deleted[*y] {
-                    return Neighborhood::One(*x);
-                } else {
-                    return Neighborhood::Two(*x, *y);
-                }
-            },
-            Neighborhood::One(x) => {
-                if self.deleted[*x] {
-                    return Neighborhood::None;
-                } else {
-                    return Neighborhood::One(*x);
+        let neighborhood = self.neighborhoods[index];
+        let mut masked_neighborhood = neighborhood.clone();
+        for (idx, maybe_neighbor) in neighborhood.iter().enumerate() {
+            if let Some(neighbor) = maybe_neighbor {
+                if self.deleted[*neighbor] {
+                    masked_neighborhood[idx] = None;
                 }
             }
-            x => { return x.clone() },
         }
+        masked_neighborhood
     }
 
     fn placement_possible(&self) -> bool {
         for i in 0..N_NODES {
-            if let Neighborhood::Two(_, _) | Neighborhood::Three(_, _, _) = self.get(i) {
+            let neighborhood = self.get(i);
+            if neighborhood.iter().filter(|n| n.is_some()).count() >= 2 {
                 return true;
             }
         }
-        return false
+        return false;
     }
 
     fn count_tilings(&mut self) -> u32 {
@@ -210,21 +148,27 @@ impl Board {
         let mut output = [None; 9];
         let mut n_tiles = 0;
 
-        for &neighbor in self.get(pick).iter() {
-            // inclusion forcing tiles
-            for &other_neighbor in self.get(pick).iter() {
-                if neighbor < other_neighbor {
-                    output[n_tiles] = Some((neighbor, pick, other_neighbor));
-                    n_tiles += 1;
+        for &maybe_neighbor in self.get(pick).iter() {
+            if let Some(neighbor) = maybe_neighbor {
+                // inclusion forcing tiles
+                for &maybe_other_neighbor in self.get(pick).iter() {
+                    if let Some(other_neighbor) = maybe_other_neighbor {
+                        if neighbor < other_neighbor {
+                            output[n_tiles] = Some((neighbor, pick, other_neighbor));
+                            n_tiles += 1;
+                        }
+                    }
                 }
-            }
 
-            // exclusion forcing tiles
-            let neighborhood_neighborhood = self.get(neighbor);
-            for &second_order_neighbor in neighborhood_neighborhood.iter() {
-                if second_order_neighbor != pick {
-                    output[n_tiles] = Some((pick, neighbor, second_order_neighbor));
-                    n_tiles += 1;
+                // exclusion forcing tiles
+                let neighborhood_neighborhood = self.get(neighbor);
+                for &maybe_second_order_neighbor in neighborhood_neighborhood.iter() {
+                    if let Some(second_order_neighbor) = maybe_second_order_neighbor {
+                        if second_order_neighbor != pick {
+                            output[n_tiles] = Some((pick, neighbor, second_order_neighbor));
+                            n_tiles += 1;
+                        }
+                    }
                 }
             }
         }
@@ -237,7 +181,13 @@ impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for i in 0..N_NODES {
             if !self.deleted[i] {
-                writeln!(f, "{} -> {}", i, self.get(i))?;
+                write!(f, "{} -> ", i,)?;
+                for maybe_neighbor in self.get(i) {
+                    if let Some(neighbor) = maybe_neighbor {
+                        write!(f, "{} ", neighbor)?;
+                    }
+                }
+                writeln!(f)?;
             }
         }
 
@@ -246,7 +196,9 @@ impl fmt::Display for Board {
 }
 
 fn main() {
+    let start = std::time::Instant::now();
     let mut board = Board::new();
-
-    println!("no of tilings: {}", board.count_tilings());
+    let num_tilings = board.count_tilings();
+    let elapsed = start.elapsed();
+    println!("Found {} tilings in {:?}", num_tilings, elapsed);
 }
